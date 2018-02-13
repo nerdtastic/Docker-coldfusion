9226fd46cb9564e40cec3d17dbc43ad21f786ef1
Thomas Pikes <pikesth@gvsu.edu>
2018-02-13 14:00:13 -0500

    added apache userId to be able to upload files and such

--- build/assets/start.sh
+++ build/assets/start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-/usr/sbin/httpd -k start
-echo 'Apache started'
-/opt/coldfusion2016/cfusion/bin/coldfusion start
-
 /usr/sbin/httpd -k stop
 echo 'Apache stopped'
 /opt/coldfusion2016/cfusion/bin/coldfusion stop
